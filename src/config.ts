02dc86b59e1c0a978466d582e894a978f5c78f2b
ksimuk <maxim.kraev@gmail.com>
2022-05-20 10:56:14 +0100

    extension examples

--- src/config.ts
+++ src/config.ts
@@ -31,42 +31,49 @@ async function main() {
     const penalties: Penalty[] = []
     if (assignment.name.startsWith('Homework:')) { 
       // Homework can be up to 3 days late with a 10% penalty and up to 7 days late with a 30% penalty
+      const dueDate = new Date(settings.endTime)
+      dueDate.setDate(dueDate.getDate() - 10) // set Due Date 10 days before the end final date
 
       penalties.push({
         id: 1,
         percent: 10,
-        datetime: setDate(settings.endTime, -7),
+        datetime: setDate(dueDate, 3),
         message: '10%'
       })
       penalties.push({
         id: 2,
         percent: 30,
-        datetime: setDate(settings.endTime, -4),
+        datetime: setDate(dueDate, 7),
         message: '30%'
       })
     } else if (assignment.name.startsWith('Labs:')) {
       // Labs can be up to 3 days late with a 5% penalty for every 12 hours the submission is late
+      
+      const dueDate = new Date(settings.endTime)
+      dueDate.setDate(dueDate.getDate() - 10) // set Due Date 10 days before the end final date
 
       let percent = 5
-      for (let shift = -3 * 24; shift < 0; shift += 12) {
+      for (let shift = 0; shift <= 3 * 24; shift += 12) {
         penalties.push({
           id: shift,
           percent,
-          datetime: setDate(settings.endTime, 0, shift),
+          datetime: setDate(dueDate, 0, shift),
           message: `${percent}%`
         })
         percent += 5
       }
     } else if (assignment.name.startsWith('Project:')) {
       // Projects can be up to 1 day late with a 1% penalty for every hour the submission is late
-      const range = _.range(-24, 0, 1)
+      const dueDate = new Date(settings.endTime)
+      dueDate.setDate(dueDate.getDate() - 1) // set Due Date 1 days before the end final date
+
       let percent = 1
       let i = 1
-      for (let shift = -24; shift < 0; shift++) {
+      for (let shift = 0; shift <= 24; shift++) {
         penalties.push({
           id: i,
           percent,
-          datetime: setDate(settings.endTime, 0, shift),
+          datetime: setDate(dueDate, 0, shift),
           message: `${percent}%`
         })
         i++
